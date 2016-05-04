6bffdb719a67a18e9c8d149880b84a5abc0b155b
Euter2 <filipmar.mf@gmail.com>
2016-05-04 14:46:32 +0200

    Update suntracker.py

--- suntracker.py
+++ suntracker.py
@@ -2,14 +2,19 @@ from crontab import CronTab
 from datetime import datetime
 from astral import Location
 
+# Location info for sunrise and sun calculation
 info=( 'Brno', 'Czech Republic', 49.241921, 16.566693, 'Europe/Prague', 278 )
 l=Location(info)
 
+# Calculate sunrise and sunset for current day
 brno_today_sunrise=l.sunrise(datetime.today().date(), True)
 brno_today_sunset=l.sunset(datetime.today().date(), True)
 
+# Open crontab file
 file_cron=CronTab(tabfile='crontab.txt')
 
+# Find open and close job in crontab and update 
+# sunrise and sunset with calculated time 
 for job in file_cron:
     if 'open command to window' in job.comment:
         job.hour.on(brno_today_sunrise.hour)
@@ -18,4 +23,5 @@ for job in file_cron:
         job.hour.on(brno_today_sunset.hour)
         job.minute.on(brno_today_sunset.minute)
 
+# Write changes to crontab file
 file_cron.write()
